2b730603d7670ac43bb0938c6a044cd9f26d8c48
caojian <sworddrmcircle@sina.com>
2019-07-11 18:09:25 +0800

    优化optionMap取值方式,不要每次从文件中取,改为直接从内存中读取,提高效率

--- src/main/java/com/inca/utils/OptionMap.java
+++ src/main/java/com/inca/utils/OptionMap.java
@@ -7,41 +7,57 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
 public class OptionMap {
-	
+	static Logger log = LoggerFactory.getLogger(OptionMap.class);
+    private static Map<String,Map<Integer,String>> optionMap = new HashMap<>();
 	public static  String getValue(String key,Integer value) {
 		if(StringUtils.isEmpty(key)||StringUtils.isEmpty(value)){
 			return "";
 		}
-		Properties properties = new Properties();
-	    // 使用ClassLoader加载properties配置文件生成对应的输入流
-	    InputStream in = OptionMap.class.getClassLoader().getResourceAsStream("option.properties");
-	    // 使用properties对象加载输入流
-	    try {
-			properties.load(in);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    //获取key对应的value值
-	    String property = properties.getProperty(key);
-	    Map<Integer,String> map = new HashMap<>();
-	    if(!StringUtils.isEmpty(property)){
-	    	try {
-				property = new String(property.getBytes("ISO-8859-1"),"utf-8");
-			} catch (UnsupportedEncodingException e) {
+		if(optionMap.get(key) != null){
+			//先从内存中取,没有的话再从文件中取
+	        Map<Integer,String> map = optionMap.get(key);
+	        if(map == null){
+	        	return "未定义";
+	        }
+		    return map.get(value)==null?"未定义":map.get(value);
+		}else{
+			Properties properties = new Properties();
+		    // 使用ClassLoader加载properties配置文件生成对应的输入流
+		    InputStream in = OptionMap.class.getClassLoader().getResourceAsStream("option.properties");
+		    // 使用properties对象加载输入流
+		    try {
+				properties.load(in);
+				 //获取key对应的value值
+			    String property = properties.getProperty(key);
+		        Map<Integer,String> map = new HashMap<>();
+			    if(!StringUtils.isEmpty(property)){
+			    	try {
+						property = new String(property.getBytes("ISO-8859-1"),"utf-8");
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+				    String[] key_values = property.split(",");
+				    for(String key_value:key_values ){
+				    	String[] kv = key_value.split(":");
+				    	map.put(Integer.valueOf(kv[0]), kv[1]);
+				    }
+			    }
+			    optionMap.put(key, map);
+			    String view = map.get(value)==null?"未定义":map.get(value);
+			    return view;
+			} catch (IOException e) {
+				log.error("get option error:"+e.getMessage());
 				e.printStackTrace();
 			}
-		    String[] key_values = property.split(",");
-		    for(String key_value:key_values ){
-		    	String[] kv = key_value.split(":");
-		    	map.put(Integer.valueOf(kv[0]), kv[1]);
-		    }
-	    }
-	    String view = map.get(value)==null?"未定义":map.get(value);
-	    return view;
+		   
+		}
+		return String.valueOf(value);
+		
 	}
 	public static void main(String[] args) throws IOException {
 		System.out.println(OptionMap.getValue("customertype",2));
