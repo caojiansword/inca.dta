e46a33efddd79050cccc4cd104f0c35bac9f20d0
zhangjingjing <zhangjingjing@inca.com.cn>
2019-07-19 19:31:52 +0800

    Signed-off-by: zhangjingjing <zhangjingjing@inca.com.cn>

--- src/main/java/com/inca/controller/customer/CustomerControler.java
+++ src/main/java/com/inca/controller/customer/CustomerControler.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSONObject;
 import com.inca.entity.pub.Customer;
 import com.inca.entity.pub.view.CustomerView;
+import com.inca.result.PageResult;
 import com.inca.service.CustomerService;
 import com.inca.service.OptionNewService;
 import com.inca.utils.OptionMap;
@@ -52,28 +53,23 @@ public class CustomerControler extends ExcelController<CustomerView>{
     }
 	@RequestMapping(value="/search")
 	@ResponseBody
-	public  List<CustomerView> search(String keyword,String type,String status){
-		List<CustomerView> customers = customerService.getCustomerList();
+	public  PageResult<CustomerView> search(String keyword,String type,String status){
+		PageResult<CustomerView> customers = customerService.getCustomerList();
 		if(!StringUtils.isEmpty(keyword)){
 			//编码 名称精确匹配
 			//customers=customers.stream().filter(p->p.getCustomerName().equals(keyword)||p.getCustomerCode().equals(keyword)).collect(Collectors.toList());
 			//模糊匹配
 			customers = customerService.getCustomerListByKeyWord(keyword);
 		}
-		// 客户类型
-		if (!StringUtils.isEmpty(type)) {
+		//客户类型
+		if(!StringUtils.isEmpty(type)){
 			Integer typeInt = Integer.valueOf(type);
-			if (customers != null) {
-				customers = customers.stream().filter(p -> typeInt.equals(p.getType())).collect(Collectors.toList());
-			}
+			customers=(PageResult<CustomerView>) customers.getData().stream().filter(p->p.getType().equals(typeInt)).collect(Collectors.toList());
 		}
-		// 状态
-		if (!StringUtils.isEmpty(status)) {
+		//状态
+		if(!StringUtils.isEmpty(status)){
 			Integer statusInt = Integer.valueOf(status);
-			if (customers != null) {
-				customers = customers.stream().filter(p -> statusInt.equals(p.getStatus()))
-						.collect(Collectors.toList());
-			}
+			customers=(PageResult<CustomerView>) customers.getData().stream().filter(p->p.getStatus().equals(statusInt)).collect(Collectors.toList());
 		}
 		return customers;
 	}
